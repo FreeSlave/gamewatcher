726e702dd3eb2bfa4de6e849be55c84ad81dfac7
Roman Chistokhodov <freeslave93@gmail.com>
2017-03-17 14:46:44 +0300

    Add license

--- source/json.d
+++ source/json.d
@@ -1,3 +1,11 @@
+/**
+ * Authors: 
+ *  $(LINK2 https://github.com/FreeSlave, Roman Chistokhodov)
+ * Copyright:
+ *  Roman Chistokhodov, 2017
+ * License: 
+ *  $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ */
 import vibe.data.json;
 
 import serverinfo;
